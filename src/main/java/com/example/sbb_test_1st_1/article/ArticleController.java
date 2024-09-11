78a1767cbc7c4acbcad70ddabfa6d7e61c2ea8f7
dudezi <a01036952858@gmail.com>
2024-09-11 12:00:12 +0900

    feat:등록버튼 추가

--- src/main/java/com/example/sbb_test_1st_1/article/ArticleController.java
+++ src/main/java/com/example/sbb_test_1st_1/article/ArticleController.java
@@ -4,18 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/article")
 @RequiredArgsConstructor
 @Controller
 public class ArticleController {
     private final ArticleService articleService;
 
-    @GetMapping("/article/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Article> articleList = this.articleService.getList();
         model.addAttribute("articleList", articleList);
         return "article_list";
+    }
+
+    @GetMapping("/create")
+    public String createArticle() {
+        return "article_form";
     }
 }
