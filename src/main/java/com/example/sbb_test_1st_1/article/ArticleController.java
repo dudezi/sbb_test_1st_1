5ffded398bcd6b1b197dee7b1fc1ff154fa751c9
JeaHukLim <a01036952858@gmail.com>
2024-09-13 01:17:44 +0900

    feat:form기능구현

--- src/main/java/com/example/sbb_test_1st_1/article/ArticleController.java
+++ src/main/java/com/example/sbb_test_1st_1/article/ArticleController.java
@@ -1,10 +1,14 @@
 package com.example.sbb_test_1st_1.article;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,7 +26,17 @@ public class ArticleController {
     }
 
     @GetMapping("/create")
-    public String createArticle() {
+    public String articleCreate(ArticleForm articleForm) {
         return "article_form";
+    }
+
+    @PostMapping("/create")
+    public String articleCreate(@Valid ArticleForm articleForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            return "article_form";
+        }
+        this.articleService.create(articleForm.getTitle(), articleForm.getContent());
+
+        return "redirect:/article/list";
     }
 }
--- src/main/java/com/example/sbb_test_1st_1/article/ArticleService.java
+++ src/main/java/com/example/sbb_test_1st_1/article/ArticleService.java
@@ -3,6 +3,7 @@ package com.example.sbb_test_1st_1.article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -12,5 +13,13 @@ public class ArticleService {
 
     public List<Article> getList() {
         return this.articleRepositoy.findAll();
+    }
+
+    public void create(String title, String content) {
+        Article a = new Article();
+        a.setTitle(title);
+        a.setContent(content);
+        a.setCreatedDate(LocalDateTime.now());
+        this.articleRepositoy.save(a);
     }
 }
